baa2ed22fb4ba17e64ba23b100db712798d4f241
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-18 13:40:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const r = document.getElementById('radius');
 const v = document.getElementById('volume');
-function volume_sphere() {
+function volumeSphere() {
     //Write your code here
-	v.value = r.value*r.value*Math.PI;
+	v.value = (r.value*r.value*r.value*Math.PI*4)/3;
 } 
 
-//window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
